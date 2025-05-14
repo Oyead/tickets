81469c0002301f5d3ea4bdeef2564bcb008213d7
eyad <eyadswailam12345@gmail.com>
2025-05-14 03:21:31 +0300

    fix: update base path and homepage for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TicketSuccess from './components/TicketSuccess';
 
 function App() {
   return (
     <Router basename="/tickets">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* your other routes */}
+        <Route path="/" element={<FormMain/>} />
+        <Route path='ticket' element={<TicketSuccess/>} />
       </Routes>
     </Router>
   );
